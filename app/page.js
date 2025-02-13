de2f04ef186fd7d09c996b19e36d21b99a0213a6
HermesThemes <momoboogeyman2000@gmail.com>
2025-02-13 15:18:30 +0000

    update

--- app/page.js
+++ app/page.js
@@ -13,20 +13,19 @@ export default function Home() {
         <div className="flex flex-col">
           <p className="text-3xl font-bold bg-gradient-to-b from-white to-zinc-400 inline-block text-transparent bg-clip-text">M'barek ETTALEBY</p>
           <p className="font-bold text-4xl bg-gradient-to-r from-violet-500 to-violet-800 inline-block text-transparent bg-clip-text"><span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-zinc-400">A</span> Full Stack Web <span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-zinc-400">Developer</span></p>
-          <div className="pt-4 flex items-center">
+          <div className="pt-4 flex gap-3 flex-wrap items-center">
             <div className="flex items-center gap-2">
               <Github size={30}/>
               <p className="text-zinc-400 text-sm">/mbarekTrismegistus</p>
+              <Divider orientation="vertical" className="bg-zinc-400 min-h-[20px]"/>
             </div>
-            <p className="text-zinc-400 text-2xl font-light mb-1">|</p>
             <div className="flex items-center gap-2">
-              <Envelope size={20}/>
-              <Divider orientation="vertical" className="b"/>
+              <Envelope size={30} className="p-[4px]"/>
               <p className="text-zinc-400 text-sm">mbarek.talbi666@gmail.com</p>
+              <Divider orientation="vertical" className="bg-zinc-400 min-h-[20px]"/>
             </div>
-            <p className="text-zinc-400 text-2xl font-light mb-1">|</p>
             <div className="flex items-center gap-2">
-              <Telephone size={20}/>
+              <Telephone size={30} className="p-[4px]"/>
               <p className="text-zinc-400 text-sm">+2126 96 428456</p>
             </div>
           </div>
@@ -39,7 +38,7 @@ export default function Home() {
             <p className="text-4xl font-bold bg-gradient-to-b from-zinc-700 to-zinc-900 inline-block text-transparent bg-clip-text">Formation</p>
           </div>
           <div className="flex">
-            <div className="px-3 text-zinc-900">
+            <div className="basis-2/4 px-3 text-zinc-900">
               <ul>
                 <li>
                   <div className="py-2">
@@ -58,7 +57,7 @@ export default function Home() {
                 </li>
               </ul>
             </div>
-            <div className="px-3 text-zinc-900">
+            <div className="basis-2/4 px-3 text-zinc-900">
               <ul>
                 <li>
                   <div className="py-2">
@@ -76,29 +75,30 @@ export default function Home() {
             <MortarboardFill size={30} color="#252525"/>
             <p className="text-4xl font-bold bg-gradient-to-b from-zinc-700 to-zinc-900 inline-block text-transparent bg-clip-text">Conaissances et compétences</p>
           </div>
-          <div className="flex text-zinc-900">
-            <div className="">
+          <div className="flex text-zinc-900 px-5">
+            <div className="basis-2/4">
+              
               <div className="px-3 py-2">
                 <p className="font-bold text-violet-800">Languages de Programmation :</p>
                 <p className="font-bold ps-3">Elixir - JavaScript/Typescript - Java - Python - PHP</p>
               </div>
               <div className="px-3 py-2">
-                <p className="font-bold text-violet-800">Système de gestion de base de données :</p>
-                <p className="font-bold ps-3">PostgreSQL - Mysql/MariaDB - MongoDB - MSSQL</p>
+                <p className="font-bold text-violet-800">Technologies Web :</p>
+                <p className="font-bold ps-3">NextJS - Phoenix Framework - ReactJS - AngularJS - Laravel - NodeJS - Bun - TailwindCSS - ExpressJS - Spring Boot</p>
+              </div>
+              <div className="px-3 py-2">
+                <p className="font-bold text-violet-800">Autre</p>
+                <p className="font-bold ps-3">Git - Administration Linux - securite web/reseau - AWS/Oracle cloud - DevOps - Cloud Native</p>
               </div>
             </div>
-            <div>
+            <div className="basis-2/4">
               <div className="px-3 py-2">
                 <p className="font-bold text-violet-800">Méthode d'analyse et de modelisation :</p>
                 <p className="font-bold ps-3">Merise, UML</p>
               </div>
               <div className="px-3 py-2">
-                <p className="font-bold text-violet-800">Technologies Web :</p>
-                <p className="font-bold ps-3">NextJS - Phoenix Framework - ReactJS - AngularJS - Laravel - NodeJS - Bun - TailwindCSS - ExpressJS</p>
-              </div>
-              <div className="px-3 py-2">
-                <p className="font-bold text-violet-800">Autre</p>
-                <p className="font-bold ps-3">Git - Administration Linux - </p>
+                <p className="font-bold text-violet-800">Système de gestion de base de données :</p>
+                <p className="font-bold ps-3">PostgreSQL - Mysql/MariaDB - MongoDB - MSSQL</p>
               </div>
             </div>
           </div>
