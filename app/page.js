d645b61986074e141fe39dc6397518a0a80ee1e9
HermesThemes <momoboogeyman2000@gmail.com>
2025-02-15 16:42:52 +0000

    update

--- app/page.js
+++ app/page.js
@@ -6,16 +6,16 @@ import { Envelope, Github, Telephone, MortarboardFill, Stars, Translate, CodeSla
 
 export default function Home() {
   return (
-    <div className="max-w-[60vw] flex flex-col">
+    <div className="max-w-[100vw] flex flex-col">
       <div className="bg-white flex justify-center p-5 items-center gap-9 bg-zinc-900">
-        <div className="flex flex-col items-center">
+        {/* <div className="flex flex-col items-center">
           <Image src={'/pierre.jpg'} width={150} height={150} alt="personal picture" className="ring-2 ring-violet-600 ring-offset-4 ring-offset-zinc-900 rounded-full aspect-square object-cover"/>
-        </div>
-        <div className="flex flex-col">
-          <p className="text-3xl font-bold bg-gradient-to-b from-white to-zinc-400 inline-block text-transparent bg-clip-text">M'barek ETTALEBY</p>
-          <p className="font-bold text-4xl bg-gradient-to-r from-violet-500 to-violet-800 inline-block text-transparent bg-clip-text"><span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-zinc-400">A</span> Full Stack Web <span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-zinc-400">Developer</span></p>
-          <div className="pt-4 flex gap-3 flex-wrap items-center">
-            <div className="flex items-center gap-2">
+        </div> */}
+        <div className="flex flex-col text-center">
+          <p className="text-4xl font-bold bg-gradient-to-b from-white to-zinc-400 inline-block text-transparent bg-clip-text">M'barek ETTALEBY</p>
+          <p className="font-bold text-5xl bg-gradient-to-r from-violet-500 to-violet-800 inline-block text-transparent bg-clip-text"><span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-zinc-400">A</span> Full Stack Web <span className="text-transparent bg-clip-text bg-gradient-to-b from-white to-zinc-400">Developer</span></p>
+          <div className="pt-4 flex gap-3 justify-center flex-wrap items-center">
+            <div className="flex  items-center gap-2">
               <Github size={30}/>
               <p className="text-zinc-400 text-sm">/mbarekTrismegistus</p>
               <Divider orientation="vertical" className="bg-zinc-400 min-h-[20px]"/>
